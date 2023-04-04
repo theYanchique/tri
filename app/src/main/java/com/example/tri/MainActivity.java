bda689401d1abc3033d5fe05233af81bedf37674
theYanchique <przegranym@bk.ru>
2023-04-04 18:17:06 +0400

    2.0

--- app/src/main/java/com/example/tri/MainActivity.java
+++ app/src/main/java/com/example/tri/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.tri;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.tri.model.ListHolidaysInfo;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -15,9 +20,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements GetData.AsyncResponse {
+public class MainActivity extends AppCompatActivity implements GetData.AsyncResponse, AdapterInfo.ListItemClickListener {
 
     private static final String TAG ="MainActivity";
+    private Toast toast;
+    private ListHolidaysInfo listHolidays;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,19 +46,45 @@ public class MainActivity extends AppCompatActivity implements GetData.AsyncResp
             JSONObject responseJSON= outputJSON.getJSONObject("response");
             JSONArray array=responseJSON.getJSONArray("holidays");
             int length=array.length();
+
+            listHolidays=new ListHolidaysInfo(length);
+
             ArrayList <String> namesHolidays= new ArrayList<String>();
             for(int i=0; i<length;i++){
                 JSONObject obj=array.getJSONObject(i);
                 String name=obj.getString("name");
+
+                JSONObject obj_date=obj.getJSONObject("date");
+                String data_iso=obj_date.getString("iso");
+
                 namesHolidays.add(name);
-                Log.d(TAG, "processFinish: "+name);
+                Log.d(TAG, "processFinish: "+name+" "+data_iso);
+                listHolidays.addHoliday(name, data_iso, i);
             }
-            ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, namesHolidays);
-            ListView list=findViewById(R.id.list);
-            list.setAdapter(adapter);
+            //ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, namesHolidays);
+            //ListView list=findViewById(R.id.list);
+            //list.setAdapter(adapter);
+            RecyclerView recyclerView=findViewById(R.id.recycler_view);
+            LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+            recyclerView.setLayoutManager(linearLayoutManager);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setAdapter(new AdapterInfo(listHolidays, length, this));
+
+
 
         }catch (JSONException e){
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void onListItemClick(int clickedItemIndex) {
+        CharSequence text=listHolidays.listHolidaysInfo[clickedItemIndex].getHoliday_name();
+        int duration=Toast.LENGTH_LONG;
+        if (toast!=null){
+            toast.cancel();
+        }
+        toast=Toast.makeText(this, text,duration);
+        toast.show();
     }
 }
---
+++ app/src/main/java/com/example/tri/model/ListHolidaysInfo.java
@@ -0,0 +1,14 @@
+package com.example.tri.model;
+
+public class ListHolidaysInfo {
+    public HolidayInfo[] listHolidaysInfo;
+
+    public ListHolidaysInfo (int size){
+        listHolidaysInfo=new HolidayInfo[size];
+    }
+    public void addHoliday (String holiday_name, String holiday_date, int id){
+        HolidayInfo holiday=new HolidayInfo(holiday_name,holiday_date);
+        listHolidaysInfo[id]=holiday;
+    }
+}
+
